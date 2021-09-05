be5111636d7b2ed86c74aeb87660139b236340b8
sechs <philippe.buteau@skema.edu>
2021-09-05 13:09:55 +0200

    server working perfectly

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var app = express();
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
-    credentials: true,
   })
 );
 
@@ -29,18 +28,18 @@ app.use("/pictures", require("./routes/pictures"));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
-  });
-  
-  // error handler
-  app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-  });
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 module.exports = app;
--- routes/pictures.js
+++ routes/pictures.js
@@ -1,40 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 const Picture = require("../models/Picture.js");
 
-router.get("/china", (req, res, next) => {
-  Picture.find({ category: "china" })
+router.get("/:id", (req, res, next) => {
+  Picture.find({ category: req.params.id })
     .then((dbResponse) => {
-      res.status(200).json(dbResponse);
-    })
-    .catch((dbErr) => {
-      res.status(500).json(dbErr);
-    });
-});
-
-router.get("/bali", (req, res, next) => {
-  Picture.find({ category: "bali" })
-    .then((dbResponse) => {
-      res.status(200).json(dbResponse);
-    })
-    .catch((dbErr) => {
-      res.status(500).json(dbErr);
-    });
-});
-
-router.get("/friends", (req, res, next) => {
-  Picture.find({ category: "friends" })
-    .then((dbResponse) => {
-      res.status(200).json(dbResponse);
-    })
-    .catch((dbErr) => {
-      res.status(500).json(dbErr);
-    });
-});
-
-router.get("/random", (req, res, next) => {
-  Picture.find({ category: "random" })
-    .then((dbResponse) => {
+      console.log(dbResponse);
       res.status(200).json(dbResponse);
     })
     .catch((dbErr) => {
